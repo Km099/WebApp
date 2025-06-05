bcb30885d2504b97509bf15a714987d96d8a1e30
Km099 <krishnag9609@gmail.com>
2025-06-05 15:14:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBocnin97y62qx8MgnSek_U278ALDO3J1U",
   authDomain: "loginwebapp-a0881.firebaseapp.com",
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "527045225230",
   appId: "1:527045225230:web:8b61c5a862ba148d47455f"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
