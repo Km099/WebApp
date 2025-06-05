7cb1d790699a39248b4c60014856cfa76f5db034
Km099 <krishnag9609@gmail.com>
2025-06-05 15:58:49 -0500

    Update post.js

--- post.js
+++ post.js
@@ -1,10 +1,11 @@
-// Firebase config (same as script.js)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID"
+  apiKey: "AIzaSyBocnin97y62qx8MgnSek_U278ALDO3J1U",
+  authDomain: "loginwebapp-a0881.firebaseapp.com",
+  projectId: "loginwebapp-a0881",
+  storageBucket: "loginwebapp-a0881.firebasestorage.app",
+  messagingSenderId: "527045225230",
+  appId: "1:527045225230:web:8b61c5a862ba148d47455f"
 };
-
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
