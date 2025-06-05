549c2a00187c93748f576c95fa8b2cd6a303f789
Km099 <krishnag9609@gmail.com>
2025-06-05 15:15:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ const firebaseConfig = {
   messagingSenderId: "527045225230",
   appId: "1:527045225230:web:8b61c5a862ba148d47455f"
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+// Signup
+function signup() {
+  const email = document.getElementById("signupEmail").value;
+  const password = document.getElementById("signupPassword").value;
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(() => alert("Signup successful!"))
+    .catch(error => alert(error.message));
+}
+
+// Login
+function login() {
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => alert("Login successful!"))
+    .catch(error => alert(error.message));
+}
